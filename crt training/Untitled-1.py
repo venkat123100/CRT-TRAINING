df21fa44e609563c4ef068d200ba08440aaf3da8
venkat123100 <venkatareddykonatham6@gmail.com>
2025-09-08 22:42:56 +0530

    Add files via upload

---
+++ crt training/Untitled-1.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the  first number"))
+num2=int(input("enter the  second number"))
+#if_else
+if(num1>num2):
+    print(num1,"is greater ")
+else:
+    print(num2,"is greater ")
---
+++ crt training/Untitled-5.py
@@ -0,0 +1,8 @@
+n=int(input("enter a number:"))
+#if_else
+if(n%3==0) and (num%5==0):
+    print("fizzbuzz")
+elif(n%3==0):
+     print("fizz")
+elif(n%5==0):
+     print("buzz")
